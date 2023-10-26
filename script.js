c437c593166470e4b6282fc758a0d322e704e458
pabloarca <31969557+pabloarca@users.noreply.github.com>
2023-10-26 10:13:19 +0200

    2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZXN0YWMzNDMiLCJhIjoiY2xibWdzYjJqMDVmcDNubWZ2M
 // Inicializar el mapa
 var map = new mapboxgl.Map({
   container: 'map', // ID del contenedor del mapa
-  style: 'mapbox://styles/estac343/clkwjs1mx004o01qpb97igu40', // Estilo del mapa (cambiar si se desea otro estilo)
+  style: 'mapbox://styles/estac343/clo6tgz8700t901qx9cg17x6f', // Estilo del mapa (cambiar si se desea otro estilo)
   center: [-5.082565, 37.539233], // Coordenadas iniciales del centro del mapa (longitud, latitud)
   zoom: 15 // Nivel de zoom inicial
 });
@@ -14,7 +14,7 @@ var map = new mapboxgl.Map({
 // Agregar interacción para mostrar el popup cuando se hace clic en un polígono
 map.on('click', function (e) {
   var features = map.queryRenderedFeatures(e.point, {
-    layers: ['buildings-9qcn2w'] // Reemplaza 'NOMBRE_DE_TU_LAYER_ID' con el ID de la capa correspondiente en Mapbox Studio
+    layers: ['ecija1-0x6fz3'] // Reemplaza 'NOMBRE_DE_TU_LAYER_ID' con el ID de la capa correspondiente en Mapbox Studio
   });
 
   if (!features.length) {
@@ -25,7 +25,18 @@ map.on('click', function (e) {
   var coordinates = e.lngLat;
 
   // Crear el contenido del popup
-  var popupContent = '<h3>Refcat: ' + feature.properties.refcat + '</h3><p><strong>Foto: </strong>' + feature.properties.foto + '</p><strong>Link: </strong>' + feature.properties.link_catas + '</p><strong>Fecha: </strong>' + feature.properties.fecha_cons + '</p><strong>Dirección: </strong>' + feature.properties.dire + '</p><strong>Tipo: </strong>' + feature.properties.categ + '</p>';
+  var popupContent = 
+  '<h3> Tipo: ' + feature.properties.tipo
+  + '</h3><strong>Refcat: </strong> ' + feature.properties.refcat 
+  + '</h3> <p><strong>Riesgo: </strong>' + feature.properties.riesgo 
+  + '</h3> <p><strong>Área afectada (m2): </strong>' + feature.properties.SUM_area 
+  + '</h3> <p><strong>Distancia más cercana (m): </strong>' + feature.properties.NEAR_DIST
+  + '</h3> <p><strong>Identificador: </strong>' + feature.properties.OBJECTID 
+  + '</p> <p><strong>Foto: </strong>' + feature.properties.foto 
+  + '</p><strong>Link a Catastro: </strong>' + feature.properties.link_catas 
+  + '</p><strong>Fecha de construcción: </strong>' + feature.properties.fecha_cons 
+  + '</p><strong>Dirección: </strong>' + feature.properties.dire 
+  + '</p><strong>Detalles: </strong>' + feature.properties.categ + '</p>';
 
 
 
